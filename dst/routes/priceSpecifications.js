1a8bca4c01ae5a4cb3995dc2898003367cc4059b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-04-05 08:15:46 +0000

    ### Added - 価格仕様管理を追加

--- dst/routes/priceSpecifications.js
+++ dst/routes/priceSpecifications.js
@@ -14,40 +14,140 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const chevre = require("@chevre/api-nodejs-client");
 const reserve_api_abstract_client_1 = require("@movieticket/reserve-api-abstract-client");
 const express_1 = require("express");
+const Message = require("../common/Const/Message");
 const priceSpecificationsRouter = express_1.Router();
-priceSpecificationsRouter.get('', (req, res) => __awaiter(this, void 0, void 0, function* () {
+priceSpecificationsRouter.get('', (_, res) => __awaiter(this, void 0, void 0, function* () {
+    res.render('priceSpecifications/index', {
+        message: '',
+        MovieTicketType: reserve_api_abstract_client_1.mvtk.util.constants.TICKET_TYPE,
+        PriceSpecificationType: chevre.factory.priceSpecificationType,
+        VideoFormatType: chevre.factory.videoFormatType,
+        SoundFormatType: chevre.factory.soundFormatType
+    });
+}));
+priceSpecificationsRouter.get('/search', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    try {
+        const priceSpecificationService = new chevre.service.PriceSpecification({
+            endpoint: process.env.API_ENDPOINT,
+            auth: req.user.authClient
+        });
+        const result = yield priceSpecificationService.search({
+            limit: Number(req.query.limit),
+            page: Number(req.query.page),
+            sort: { price: chevre.factory.sortType.Ascending },
+            typeOf: (req.query.typeOf !== '') ? req.query.typeOf : undefined,
+            appliesToMovieTicket: {
+                serviceTypes: (req.query.appliesToMovieTicketType !== '') ? [req.query.appliesToMovieTicketType] : undefined
+            },
+            appliesToVideoFormats: (req.query.appliesToVideoFormat !== '') ? [req.query.appliesToVideoFormat] : undefined,
+            appliesToSoundFormats: (req.query.appliesToSoundFormat !== '') ? [req.query.appliesToSoundFormat] : undefined
+        });
+        res.json({
+            success: true,
+            count: result.totalCount,
+            results: result.data
+        });
+    }
+    catch (error) {
+        res.json({
+            success: false,
+            message: error.message,
+            count: 0,
+            results: []
+        });
+    }
+}));
+priceSpecificationsRouter.all('/new', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    let message = '';
+    let errors = {};
+    if (req.method === 'POST') {
+        // バリデーション
+        validate(req);
+        const validatorResult = yield req.getValidationResult();
+        errors = req.validationErrors(true);
+        if (validatorResult.isEmpty()) {
+            try {
+                let priceSpecification = createMovieFromBody(req.body);
+                const priceSpecificationService = new chevre.service.PriceSpecification({
+                    endpoint: process.env.API_ENDPOINT,
+                    auth: req.user.authClient
+                });
+                priceSpecification = yield priceSpecificationService.create(priceSpecification);
+                req.flash('message', '登録しました');
+                res.redirect(`/priceSpecifications/${priceSpecification.id}/update`);
+                return;
+            }
+            catch (error) {
+                message = error.message;
+            }
+        }
+    }
+    const forms = Object.assign({}, req.body);
+    res.render('priceSpecifications/new', {
+        message: message,
+        errors: errors,
+        forms: forms,
+        MovieTicketType: reserve_api_abstract_client_1.mvtk.util.constants.TICKET_TYPE,
+        PriceSpecificationType: chevre.factory.priceSpecificationType,
+        VideoFormatType: chevre.factory.videoFormatType,
+        SoundFormatType: chevre.factory.soundFormatType
+    });
+}));
+priceSpecificationsRouter.all('/:id/update', (req, res) => __awaiter(this, void 0, void 0, function* () {
+    let message = '';
+    let errors = {};
     const priceSpecificationService = new chevre.service.PriceSpecification({
         endpoint: process.env.API_ENDPOINT,
         auth: req.user.authClient
     });
-    const searchVideoFormatCompoundPriceSpecificationsResult = yield priceSpecificationService.searchCompoundPriceSpecifications({
-        limit: 1,
-        page: 1,
-        typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-        priceComponent: { typeOf: chevre.factory.priceSpecificationType.VideoFormatChargeSpecification }
+    let priceSpecification = yield priceSpecificationService.findById({
+        id: req.params.id
     });
-    const searchSoundFormatCompoundPriceSpecificationsResult = yield priceSpecificationService.searchCompoundPriceSpecifications({
-        limit: 1,
-        page: 1,
-        typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-        priceComponent: { typeOf: chevre.factory.priceSpecificationType.SoundFormatChargeSpecification }
-    });
-    const searchMovieTicketTypeChargeCompoundPriceSpecificationsResult = yield priceSpecificationService.searchCompoundPriceSpecifications({
-        limit: 1,
-        page: 1,
-        typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-        priceComponent: { typeOf: chevre.factory.priceSpecificationType.MovieTicketTypeChargeSpecification }
-    });
-    const movieTicketTypeChargeSpecifications = searchMovieTicketTypeChargeCompoundPriceSpecificationsResult.data[0].priceComponent;
-    const movieTicketTypeCodes = Array.from(new Set(movieTicketTypeChargeSpecifications.map((s) => s.appliesToMovieTicketType)));
-    res.render('priceSpecifications/index', {
-        message: '',
-        videoFormaChargeSpecifications: searchVideoFormatCompoundPriceSpecificationsResult.data[0].priceComponent,
-        soundFormaChargeSpecifications: searchSoundFormatCompoundPriceSpecificationsResult.data[0].priceComponent,
-        movieTicketTypeChargeSpecifications: searchMovieTicketTypeChargeCompoundPriceSpecificationsResult.data[0].priceComponent,
-        movieTicketTypeCodes: movieTicketTypeCodes,
-        movieTicketTypes: reserve_api_abstract_client_1.mvtk.util.constants.TICKET_TYPE,
-        videoFormatType: chevre.factory.videoFormatType
+    if (req.method === 'POST') {
+        // バリデーション
+        validate(req);
+        const validatorResult = yield req.getValidationResult();
+        errors = req.validationErrors(true);
+        if (validatorResult.isEmpty()) {
+            // 作品DB登録
+            try {
+                priceSpecification = Object.assign({}, createMovieFromBody(req.body), { id: priceSpecification.id });
+                yield priceSpecificationService.update(priceSpecification);
+                req.flash('message', '更新しました');
+                res.redirect(req.originalUrl);
+                return;
+            }
+            catch (error) {
+                message = error.message;
+            }
+        }
+    }
+    const forms = Object.assign({}, priceSpecification, req.body);
+    res.render('priceSpecifications/update', {
+        message: message,
+        errors: errors,
+        forms: forms,
+        MovieTicketType: reserve_api_abstract_client_1.mvtk.util.constants.TICKET_TYPE,
+        PriceSpecificationType: chevre.factory.priceSpecificationType,
+        VideoFormatType: chevre.factory.videoFormatType,
+        SoundFormatType: chevre.factory.soundFormatType
     });
 }));
+function createMovieFromBody(body) {
+    return {
+        typeOf: body.typeOf,
+        price: Number(body.price),
+        priceCurrency: chevre.factory.priceCurrency.JPY,
+        appliesToVideoFormat: body.appliesToVideoFormat,
+        appliesToSoundFormat: body.appliesToSoundFormat,
+        appliesToMovieTicketType: body.appliesToMovieTicketType,
+        valueAddedTaxIncluded: true
+    };
+}
+function validate(req) {
+    let colName = '';
+    colName = '価格仕様タイプ';
+    req.checkBody('typeOf', Message.Common.required.replace('$fieldName$', colName)).notEmpty();
+    // req.checkBody('name', Message.Common.getMaxLength(colName, NAME_MAX_LENGTH_CODE)).len({ max: NAME_MAX_LENGTH_NAME_JA });
+}
 exports.default = priceSpecificationsRouter;
--- src/routes/priceSpecifications.ts
+++ src/routes/priceSpecifications.ts
@@ -3,46 +3,176 @@
  */
 import * as chevre from '@chevre/api-nodejs-client';
 import { mvtk } from '@movieticket/reserve-api-abstract-client';
-import { Router } from 'express';
+import { Request, Router } from 'express';
+
+import * as Message from '../common/Const/Message';
 
 const priceSpecificationsRouter = Router();
+
 priceSpecificationsRouter.get(
     '',
+    async (_, res) => {
+        res.render('priceSpecifications/index', {
+            message: '',
+            MovieTicketType: mvtk.util.constants.TICKET_TYPE,
+            PriceSpecificationType: chevre.factory.priceSpecificationType,
+            VideoFormatType: chevre.factory.videoFormatType,
+            SoundFormatType: chevre.factory.soundFormatType
+        });
+    }
+);
+
+priceSpecificationsRouter.get(
+    '/search',
     async (req, res) => {
+        try {
+            const priceSpecificationService = new chevre.service.PriceSpecification({
+                endpoint: <string>process.env.API_ENDPOINT,
+                auth: req.user.authClient
+            });
+            const result = await priceSpecificationService.search({
+                limit: Number(req.query.limit),
+                page: Number(req.query.page),
+                sort: { price: chevre.factory.sortType.Ascending },
+                typeOf: (req.query.typeOf !== '') ? req.query.typeOf : undefined,
+                appliesToMovieTicket: {
+                    serviceTypes: (req.query.appliesToMovieTicketType !== '') ? [req.query.appliesToMovieTicketType] : undefined
+                },
+                appliesToVideoFormats: (req.query.appliesToVideoFormat !== '') ? [req.query.appliesToVideoFormat] : undefined,
+                appliesToSoundFormats: (req.query.appliesToSoundFormat !== '') ? [req.query.appliesToSoundFormat] : undefined
+            });
+
+            res.json({
+                success: true,
+                count: result.totalCount,
+                results: result.data
+            });
+        } catch (error) {
+            res.json({
+                success: false,
+                message: error.message,
+                count: 0,
+                results: []
+            });
+        }
+    }
+);
+
+priceSpecificationsRouter.all(
+    '/new',
+    async (req, res) => {
+        let message = '';
+        let errors: any = {};
+
+        if (req.method === 'POST') {
+            // バリデーション
+            validate(req);
+            const validatorResult = await req.getValidationResult();
+            errors = req.validationErrors(true);
+            if (validatorResult.isEmpty()) {
+                try {
+                    let priceSpecification = createMovieFromBody(req.body);
+                    const priceSpecificationService = new chevre.service.PriceSpecification({
+                        endpoint: <string>process.env.API_ENDPOINT,
+                        auth: req.user.authClient
+                    });
+                    priceSpecification = await priceSpecificationService.create(priceSpecification);
+
+                    req.flash('message', '登録しました');
+                    res.redirect(`/priceSpecifications/${priceSpecification.id}/update`);
+
+                    return;
+                } catch (error) {
+                    message = error.message;
+                }
+            }
+        }
+
+        const forms = {
+            ...req.body
+        };
+
+        res.render('priceSpecifications/new', {
+            message: message,
+            errors: errors,
+            forms: forms,
+            MovieTicketType: mvtk.util.constants.TICKET_TYPE,
+            PriceSpecificationType: chevre.factory.priceSpecificationType,
+            VideoFormatType: chevre.factory.videoFormatType,
+            SoundFormatType: chevre.factory.soundFormatType
+        });
+    }
+);
+
+priceSpecificationsRouter.all(
+    '/:id/update',
+    async (req, res) => {
+        let message = '';
+        let errors: any = {};
+
         const priceSpecificationService = new chevre.service.PriceSpecification({
             endpoint: <string>process.env.API_ENDPOINT,
             auth: req.user.authClient
         });
-        const searchVideoFormatCompoundPriceSpecificationsResult = await priceSpecificationService.searchCompoundPriceSpecifications({
-            limit: 1,
-            page: 1,
-            typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-            priceComponent: { typeOf: chevre.factory.priceSpecificationType.VideoFormatChargeSpecification }
+        let priceSpecification = await priceSpecificationService.findById({
+            id: req.params.id
         });
-        const searchSoundFormatCompoundPriceSpecificationsResult = await priceSpecificationService.searchCompoundPriceSpecifications({
-            limit: 1,
-            page: 1,
-            typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-            priceComponent: { typeOf: chevre.factory.priceSpecificationType.SoundFormatChargeSpecification }
-        });
-        const searchMovieTicketTypeChargeCompoundPriceSpecificationsResult =
-            await priceSpecificationService.searchCompoundPriceSpecifications({
-                limit: 1,
-                page: 1,
-                typeOf: chevre.factory.priceSpecificationType.CompoundPriceSpecification,
-                priceComponent: { typeOf: chevre.factory.priceSpecificationType.MovieTicketTypeChargeSpecification }
-            });
-        const movieTicketTypeChargeSpecifications = searchMovieTicketTypeChargeCompoundPriceSpecificationsResult.data[0].priceComponent;
-        const movieTicketTypeCodes = Array.from(new Set(movieTicketTypeChargeSpecifications.map((s) => s.appliesToMovieTicketType)));
-        res.render('priceSpecifications/index', {
-            message: '',
-            videoFormaChargeSpecifications: searchVideoFormatCompoundPriceSpecificationsResult.data[0].priceComponent,
-            soundFormaChargeSpecifications: searchSoundFormatCompoundPriceSpecificationsResult.data[0].priceComponent,
-            movieTicketTypeChargeSpecifications: searchMovieTicketTypeChargeCompoundPriceSpecificationsResult.data[0].priceComponent,
-            movieTicketTypeCodes: movieTicketTypeCodes,
-            movieTicketTypes: mvtk.util.constants.TICKET_TYPE,
-            videoFormatType: chevre.factory.videoFormatType
+
+        if (req.method === 'POST') {
+            // バリデーション
+            validate(req);
+            const validatorResult = await req.getValidationResult();
+            errors = req.validationErrors(true);
+            if (validatorResult.isEmpty()) {
+                // 作品DB登録
+                try {
+                    priceSpecification = { ...createMovieFromBody(req.body), id: priceSpecification.id };
+                    await priceSpecificationService.update(priceSpecification);
+                    req.flash('message', '更新しました');
+                    res.redirect(req.originalUrl);
+
+                    return;
+                } catch (error) {
+                    message = error.message;
+                }
+            }
+        }
+
+        const forms = {
+            ...priceSpecification,
+            ...req.body
+        };
+
+        res.render('priceSpecifications/update', {
+            message: message,
+            errors: errors,
+            forms: forms,
+            MovieTicketType: mvtk.util.constants.TICKET_TYPE,
+            PriceSpecificationType: chevre.factory.priceSpecificationType,
+            VideoFormatType: chevre.factory.videoFormatType,
+            SoundFormatType: chevre.factory.soundFormatType
         });
     }
 );
+
+function createMovieFromBody(body: any): chevre.factory.priceSpecification.IPriceSpecification<any> {
+    return {
+        typeOf: body.typeOf,
+        price: Number(body.price),
+        priceCurrency: chevre.factory.priceCurrency.JPY,
+        appliesToVideoFormat: body.appliesToVideoFormat,
+        appliesToSoundFormat: body.appliesToSoundFormat,
+        appliesToMovieTicketType: body.appliesToMovieTicketType,
+        valueAddedTaxIncluded: true
+    };
+}
+
+function validate(req: Request): void {
+    let colName: string = '';
+
+    colName = '価格仕様タイプ';
+    req.checkBody('typeOf', Message.Common.required.replace('$fieldName$', colName)).notEmpty();
+    // req.checkBody('name', Message.Common.getMaxLength(colName, NAME_MAX_LENGTH_CODE)).len({ max: NAME_MAX_LENGTH_NAME_JA });
+}
+
 export default priceSpecificationsRouter;
