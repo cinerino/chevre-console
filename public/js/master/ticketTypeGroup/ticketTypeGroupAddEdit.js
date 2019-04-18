4363affe6b89fa44742c8699841f50ebc9943447
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-04-18 18:43:25 +0900

    ### Fixed - 券種グループ、券種売上金額変更修正 - 追加特性UI修正

--- public/js/master/ticketTypeGroup/ticketTypeGroupAddEdit.js
+++ public/js/master/ticketTypeGroup/ticketTypeGroupAddEdit.js
@@ -1,28 +1,128 @@
-$(function () {
-    var ticketTypeGroupId = $('input[name="id"]').val();
+var ticketTypeGroupId = '';
+var form = {
+    price: ''
+};
 
-    $('body').on('change', '#price', function () {
-        var price = $(this).val();
-        var url = '/tickettypegroups/getTicketTypePriceList';
-        // 対象券種名の処理
-        var ticketTypeArray = [];
-        $('#sortable2 > li').each(function () {
-            var uid = $(this).attr('uid');
-            ticketTypeArray.push(uid);
-        });
-        $('#sortable1').empty();
+$(function () {
+
+    ticketTypeGroupId = $('input[name="id"]').val();
+
+    $('#price').on('change', priceChange);
+
+    $("#sortable1, #sortable2").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+
+    // show or hide 対象券種名
+    var priceList = $('#sortable2 > li').length;
+    if (priceList == 0) {
+        // $('#sortable1').hide();
+        // $('#sortable2').hide();
+    }
+
+    // form submit
+    $('.btn-ok').on('click', submit);
+
+    // 削除ボタン
+    $('.btn-delete').on('click', remove);
+});
+
+/**
+ * 削除
+ */
+function remove() {
+    if (window.confirm('元には戻せません。本当に削除しますか？')) {
         $.ajax({
             dataType: 'json',
-            url: url,
-            cache: false,
-            type: 'GET',
-            data: {
-                price: Number(price)
-            },
-            beforeSend: function () {
-                $('#price').prop('disabled', true);
+            url: '/tickettypegroups/' + ticketTypeGroupId,
+            type: 'DELETE'
+        }).done(function () {
+            alert('削除しました');
+            location.href = '/ticketTypeGroups';
+        }).fail(function (jqxhr, textStatus, error) {
+            var message = '削除できませんでした';
+            if (jqxhr.responseJSON != undefined && jqxhr.responseJSON.error != undefined) {
+                message += ': ' + jqxhr.responseJSON.error.message;
             }
-        }).done(function (data) {
+            alert(message);
+        }).always(function () {
+        });
+    } else {
+    }
+}
+
+/**
+ * 更新
+ */
+function submit() {
+    // 券種リストに含まれるムビチケ券種区分リスト
+    var appliesToMovieTicketTypes = [];
+    // 対象券種名の処理
+    var ticketTypeIds = [];
+
+    $('#sortable2 > li').each(function () {
+        var uid = $(this).attr('uid');
+        ticketTypeIds.push(uid);
+        var appliesToMovieTicketType = $(this).attr('appliesToMovieTicketType');
+        if (appliesToMovieTicketType !== '') {
+            appliesToMovieTicketTypes.push(appliesToMovieTicketType);
+        }
+    });
+
+    ticketTypeIds.forEach(function (ticketTypeId) {
+        $('<input />').attr('type', 'hidden')
+            .attr('name', 'ticketTypes')
+            .attr('value', ticketTypeId)
+            .appendTo('#ticketTypeGroupsForm');
+    });
+
+    // ムビチケ券種区分の重複を確認
+    var uniqueAppliesToMovieTicketTypes = appliesToMovieTicketTypes.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+    });
+    if (appliesToMovieTicketTypes.length !== uniqueAppliesToMovieTicketTypes.length) {
+        alert('ムビチケ券種区分が重複しています');
+
+        return false;
+    }
+
+    $('form').submit();
+}
+
+/**
+ * 券種情報取得
+ */
+function getTicketTypePriceList(price) {
+    var options = {
+        dataType: 'json',
+        url: '/tickettypegroups/getTicketTypePriceList',
+        cache: false,
+        type: 'GET',
+        data: {
+            price: Number(price)
+        }
+    };
+    return $.ajax(options);
+}
+
+/**
+ * 券種売上金額変更
+ */
+function priceChange() {
+    var price = $(this).val();
+    if (price === '' || form.price === price) {
+        return;
+    }
+    form.price = price;
+    // 対象券種名の処理
+    var ticketTypeArray = [];
+    $('#sortable2 > li').each(function () {
+        var uid = $(this).attr('uid');
+        ticketTypeArray.push(uid);
+    });
+    $('#sortable1').empty();
+    getTicketTypePriceList(form.price)
+        .then(function (data) {
             var ticketType = data.results;
             if (data.success) {
                 var i;
@@ -46,80 +146,8 @@ $(function () {
                 }
                 $('#sortable1').show();
                 $('#sortable2').show();
-                $('#price').prop('disabled', false);
             }
-        }).fail(function (jqxhr, textStatus, error) {
-            alert('fail');
-            $('#price').prop('disabled', false);
+        }).catch(function (jqxhr, textStatus, error) {
+            alert('券種情報の取得に失敗しました');
         });
-    });
-
-    $("#sortable1, #sortable2").sortable({
-        connectWith: ".connectedSortable"
-    }).disableSelection();
-
-    // show or hide 対象券種名
-    var priceList = $('#sortable2 > li').length;
-    if (priceList == 0) {
-        // $('#sortable1').hide();
-        // $('#sortable2').hide();
-    }
-
-    // form submit
-    $('.btn-ok').on('click', function () {
-        // 券種リストに含まれるムビチケ券種区分リスト
-        var appliesToMovieTicketTypes = [];
-        // 対象券種名の処理
-        var ticketTypeIds = [];
-
-        $('#sortable2 > li').each(function () {
-            var uid = $(this).attr('uid');
-            ticketTypeIds.push(uid);
-            var appliesToMovieTicketType = $(this).attr('appliesToMovieTicketType');
-            if (appliesToMovieTicketType !== '') {
-                appliesToMovieTicketTypes.push(appliesToMovieTicketType);
-            }
-        });
-
-        ticketTypeIds.forEach(function (ticketTypeId) {
-            $('<input />').attr('type', 'hidden')
-                .attr('name', 'ticketTypes')
-                .attr('value', ticketTypeId)
-                .appendTo('#ticketTypeGroupsForm');
-        });
-
-        // ムビチケ券種区分の重複を確認
-        var uniqueAppliesToMovieTicketTypes = appliesToMovieTicketTypes.filter(function (value, index, self) {
-            return self.indexOf(value) === index;
-        });
-        if (appliesToMovieTicketTypes.length !== uniqueAppliesToMovieTicketTypes.length) {
-            alert('ムビチケ券種区分が重複しています');
-
-            return false;
-        }
-
-        $('form').submit();
-    });
-
-    // 削除ボタン
-    $('.btn-delete').on('click', function () {
-        if (window.confirm('元には戻せません。本当に削除しますか？')) {
-            $.ajax({
-                dataType: 'json',
-                url: '/tickettypegroups/' + ticketTypeGroupId,
-                type: 'DELETE'
-            }).done(function () {
-                alert('削除しました');
-                location.href = '/ticketTypeGroups';
-            }).fail(function (jqxhr, textStatus, error) {
-                var message = '削除できませんでした';
-                if (jqxhr.responseJSON != undefined && jqxhr.responseJSON.error != undefined) {
-                    message += ': ' + jqxhr.responseJSON.error.message;
-                }
-                alert(message);
-            }).always(function () {
-            });
-        } else {
-        }
-    });
-});
+}
