5d9636857ad8f6a12bd93812e61044f17a76ba3e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-31 13:12:31 +0000

    ### Changed - UI調整

--- public/js/master/places/seat.js
+++ public/js/master/places/seat.js
@@ -11,6 +11,78 @@ $(function () {
 
         remove(id);
     });
+
+    var screenBranchCodeSelection = $('#screenBranchCode');
+    screenBranchCodeSelection.select2({
+        // width: 'resolve', // need to override the changed default,
+        placeholder: 'スクリーン選択',
+        allowClear: true,
+        ajax: {
+            url: '/places/screeningRoom/search',
+            dataType: 'json',
+            data: function (params) {
+                var query = {
+                    limit: 20,
+                    page: 1,
+                    branchCode: { $regex: params.term }
+                }
+
+                // Query parameters will be ?search=[term]&type=public
+                return query;
+            },
+            delay: 250, // wait 250 milliseconds before triggering the request
+            // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+            processResults: function (data) {
+                // movieOptions = data.data;
+
+                // Transforms the top-level key of the response object from 'items' to 'results'
+                return {
+                    results: data.results.map(function (screeningRoom) {
+                        return {
+                            id: screeningRoom.branchCode,
+                            text: screeningRoom.containedInPlace.name.ja + ' ' + screeningRoom.branchCode + ' ' + screeningRoom.name.ja
+                        }
+                    })
+                };
+            }
+        }
+    });
+
+    var screeningRoomSectionBranchCodeSelection = $('#screeningRoomSectionBranchCode');
+    screeningRoomSectionBranchCodeSelection.select2({
+        // width: 'resolve', // need to override the changed default,
+        placeholder: 'セクション選択',
+        allowClear: true,
+        ajax: {
+            url: '/places/screeningRoomSection/search',
+            dataType: 'json',
+            data: function (params) {
+                var query = {
+                    limit: 20,
+                    page: 1,
+                    branchCode: { $regex: params.term }
+                }
+
+                // Query parameters will be ?search=[term]&type=public
+                return query;
+            },
+            delay: 250, // wait 250 milliseconds before triggering the request
+            // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+            processResults: function (data) {
+                // movieOptions = data.data;
+
+                // Transforms the top-level key of the response object from 'items' to 'results'
+                return {
+                    results: data.results.map(function (screeningRoomSection) {
+                        return {
+                            id: screeningRoomSection.branchCode,
+                            text: screeningRoomSection.branchCode + ' ' + screeningRoomSection.name.ja
+                        }
+                    })
+                };
+            }
+        }
+    });
 });
 
 /**
