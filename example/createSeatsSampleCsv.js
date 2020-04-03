512f254e43a4bfc0133b7e40214f6d5d0fa2c6a3
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-04-03 06:38:05 +0000

    ### Added - 座席csv登録機能を仮実装

--- example/createSeatsSampleCsv.js
+++ example/createSeatsSampleCsv.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 
 let csv = 'branchCode';
 
-for (let i = 0; i < 1000; i++) {
-    csv += `\n${`000${i}`.slice(-4)}`;
-}
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+for (var i = 0; i < alphabet.length; i++) {
+    for (let j = 0; j < 100; j++) {
+        csv += `\n${alphabet[i]}-${j + 1}`;
+    }
+};
 
 fs.writeFileSync(`${__dirname}/seatsSample.csv`, csv);
 console.log(csv);
