e0b04a9b281eae338c20383c3de734d67d682d03
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-09-02 07:58:22 +0000

    ### Added - AVAILABLE_PRODUCT_TYPES設定を追加

--- dst/factory/productType.js
+++ dst/factory/productType.js
@@ -14,7 +14,19 @@ const types = [
     { codeValue: ProductType.EventService, name: '予約サービス' }
 ];
 if (process.env.USE_OFFER_ADD_ON === '1') {
-    types.push({ codeValue: ProductType.Product, name: 'アドオン' }, { codeValue: ProductType.MembershipService, name: 'メンバーシップ' }, { codeValue: ProductType.Account, name: '口座' }, { codeValue: ProductType.PaymentCard, name: 'ペイメントカード' }
+    types.push({ codeValue: ProductType.Product, name: 'アドオン' });
+}
+const AVAILABLE_PRODUCT_TYPES = (typeof process.env.AVAILABLE_PRODUCT_TYPES === 'string')
+    ? process.env.AVAILABLE_PRODUCT_TYPES.split(',')
+    : [];
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.MembershipService)) {
+    types.push({ codeValue: ProductType.MembershipService, name: 'メンバーシップ' });
+}
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.Account)) {
+    types.push({ codeValue: ProductType.Account, name: '口座' });
+}
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.PaymentCard)) {
+    types.push({ codeValue: ProductType.PaymentCard, name: 'ペイメントカード' }
     // { codeValue: ProductType.PointCard, name: 'ポイントカード' }
     );
 }
--- src/factory/productType.ts
+++ src/factory/productType.ts
@@ -18,9 +18,26 @@ const types: IProductType[] = [
 
 if (process.env.USE_OFFER_ADD_ON === '1') {
     types.push(
-        { codeValue: ProductType.Product, name: 'アドオン' },
-        { codeValue: ProductType.MembershipService, name: 'メンバーシップ' },
-        { codeValue: ProductType.Account, name: '口座' },
+        { codeValue: ProductType.Product, name: 'アドオン' }
+    );
+}
+
+const AVAILABLE_PRODUCT_TYPES = (typeof process.env.AVAILABLE_PRODUCT_TYPES === 'string')
+    ? process.env.AVAILABLE_PRODUCT_TYPES.split(',')
+    : [];
+
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.MembershipService)) {
+    types.push(
+        { codeValue: ProductType.MembershipService, name: 'メンバーシップ' }
+    );
+}
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.Account)) {
+    types.push(
+        { codeValue: ProductType.Account, name: '口座' }
+    );
+}
+if (AVAILABLE_PRODUCT_TYPES.includes(ProductType.PaymentCard)) {
+    types.push(
         { codeValue: ProductType.PaymentCard, name: 'ペイメントカード' }
         // { codeValue: ProductType.PointCard, name: 'ポイントカード' }
     );
